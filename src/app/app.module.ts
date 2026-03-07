d2304747695c262188ad1c90854c9a6fcf997a8e
sephf <74473791+sephf@users.noreply.github.com>
2026-03-07 14:39:11 -0600

    Fix: run serve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AlertComponent } from './components/alert/alert.component';
+import { BillingPageComponent } from './pages/billing-page/billing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlertComponent
+    AlertComponent,
+    BillingPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+    CommonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/billing.service.ts
+++ src/app/services/billing.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ProductList } from '../models/product';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
